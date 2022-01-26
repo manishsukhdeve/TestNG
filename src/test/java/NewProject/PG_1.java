6ee76c301a8131fe3cc14bf88b8ee4e066233b03
manish <manish.sukhdeve@gmail.com>
2022-01-27 00:15:36 +0530

    My first jenkins commit

--- src/test/java/NewProject/PG_1.java
+++ src/test/java/NewProject/PG_1.java
@@ -6,34 +6,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PG_1 {
-	
+
 	public static void main(String[] args) {
-		
+
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		
+
 		String baseURL = "http://demo.guru99.com/test/newtours/";
 		String expectedTitle = "Welcome: Mercury Tour";
 		String actualTitle = "";
-		
+
 		driver.get(baseURL);
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
-		
+
 		actualTitle = driver.getTitle();
-		
+
 		if (actualTitle.contentEquals(actualTitle)) {
 			System.out.println("Pass");
-		}
-		else {
+		} else {
 			System.out.println("Fail");
 		}
-		
-//		System.out.println(driver.getPageSource());
-		
-//		driver.close();
+
+		System.out.println(driver.getPageSource());
+
+//		driver.close();	
 		driver.quit();
-		
+
 	}
 
 }
